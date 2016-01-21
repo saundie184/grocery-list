0a36c887320b3caf8c67b4718c8a0e053daa526f
Saundie Weiss <saundieweiss@gmail.com>
2016-01-21 09:18:14 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var app = angular.module('groceryApp', []);
+
+app.controller('NewItemController', function() {
+  var vm = this;
+  // vm.newContact = {};
+  // vm.name = '';
+  // vm.email = '';
+  // vm.phone = '';
+  //
+  vm.submit = function() {};
+});
